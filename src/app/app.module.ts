49d25374580ee4e7610c843bd4fab83c44fe2bc7
Stéphane ZUCATTI <stephane.zucatti@me.com>
2022-09-02 18:47:16 +0200

    fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { AuthComponent } from './auth/auth.component';
 import { Menu } from './menu/menu.component';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [AppComponent, AboutComponent, HomeComponent, Menu],
+  declarations: [
+    AppComponent,
+    AboutComponent,
+    HomeComponent,
+    Menu,
+    AuthComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
