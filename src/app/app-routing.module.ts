5cf2704cd1dc2794fdcafb20f13bb7a83947ab8c
Stéphane ZUCATTI <stephane.zucatti@me.com>
2022-09-02 16:09:49 +0200

    add: bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
-import { PostsComponent } from './posts/posts.component';
-import { PostComponent } from './post/post.component';
 
 // http://localhost:4200/ -> HomeComponent
 // http://localhost:4200/about -> AboutComponent
-// http://localhost:4200/posts -> PostsComponent
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
